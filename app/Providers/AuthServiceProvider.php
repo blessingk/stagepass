eed7bc63fce9dc682b1ddcd765c40fdb6b71f73b
bk <kblessingtatenda@gmail.com>
2025-05-16 23:41:37 +0200

    restructure events screen

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,21 +2,15 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
-        User::class => UserPolicy::class,
     ];
 
     public function boot(): void
     {
         $this->registerPolicies();
-
-        Gate::define('manage-users', [UserPolicy::class, 'manageUsers']);
     }
-} 
+}
